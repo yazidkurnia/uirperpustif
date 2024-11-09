8766eceb9272d547197437aeeb7ba25399dfc73d
yazidkurnia <zidyazid222@gmail.com>
2024-11-10 02:05:19 +0700

    refactor(Model): set value fillable

--- app/Models/Transaction/Transaction.php
+++ app/Models/Transaction/Transaction.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Transaction extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'userid', 'book_id', 'jenis_transaksi', 'tgl_pinjam', 'tgl_wajib_kembali'
+    ];
 }
--- app/Models/TransactionDetail/TransactionDetail.php
+++ app/Models/TransactionDetail/TransactionDetail.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class TransactionDetail extends Model
 {
     use HasFactory;
+    
+    protected $fillable = [
+        'transaction_id','book_id'
+    ];
 }
