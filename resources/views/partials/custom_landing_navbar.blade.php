1a35c9ac6eeebbe301df6165d8ead8eb6fce75f5
yazidkurnia <zidyazid222@gmail.com>
2024-12-01 13:38:49 +0700

    init(View): landing page

---
+++ resources/views/partials/custom_landing_navbar.blade.php
@@ -0,0 +1,22 @@
+<nav class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme"
+    id="layout-navbar">
+    <div class="layout-menu-toggle navbar-nav align-items-xl-center me-4 me-xl-0 d-xl-none">
+        <a class="nav-item nav-link px-0 me-xl-6" href="javascript:void(0)">
+            <i class="bx bx-menu bx-md"></i>
+        </a>
+    </div>
+    <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
+        <!-- Search -->
+        <div class="navbar-nav align-items-center">
+            <div class="nav-item">
+                {{-- <i class="bx bx-search bx-md"></i>
+                <input type="text" class="form-control border-0 shadow-none ps-1 ps-sm-2" placeholder="Search..."
+                    aria-label="Search..." /> --}}
+                <h4 class="shadow-none ps-1 ps-sm-2 py-0 my-0 text-pretty text-primary">Sistem Informasi Perpustakaan
+                    Teknik
+                    Informatika UIR</h4>
+            </div>
+        </div>
+
+    </div>
+</nav>
